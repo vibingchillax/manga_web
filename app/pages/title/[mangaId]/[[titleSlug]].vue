223763229ae80fed336d91a6f2964287d78d4736
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-08-04 23:51:49 +0700

    chore: rename urls

---
+++ app/pages/title/[mangaId]/[[titleSlug]].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { MangaDetails } from '#components';
+
+const route = useRoute()
+const { manga, pending, error } = useMangaById(route.params.mangaId as string)
+</script>
+
+<template>
+    <template v-if="pending">
+      Loading manga...
+    </template>
+
+    <template v-else-if="error">
+      Failed to load manga.
+    </template>
+
+    <MangaDetails :manga v-else-if="manga"/>
+</template>
---
+++ app/pages/title/random.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const router = useRouter()
+
+const { data, error } = await useAsyncData('random', () => getMangaRandom())
+
+if (data.value?.data) {
+  router.push(`/title/${data.value.data.id}/${toKebabCase(data.value.data.attributes?.title?.en as string)}`)
+}
+</script>
+
+<template>
+  <div>Loading random manga...</div>
+</template>
