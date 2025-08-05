86ac9d0a59e0b77fa254a43ced8721f05ebae768
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-08-05 19:08:56 +0700

    feat: add original cover image to useMangaCover

--- app/composables/relationships/useMangaCover.ts
+++ app/composables/relationships/useMangaCover.ts
@@ -1,10 +1,13 @@
 export const useMangaCover = (manga: Manga) => {
     const cover = manga.relationships?.find(r => r.type === 'cover_art');
+    const coverUrl =
+    `https://uploads.mangadex.org/covers/${manga.id}/${cover?.attributes?.fileName}`
     const coverUrl256 = 
     `https://uploads.mangadex.org/covers/${manga.id}/${cover?.attributes?.fileName}.256.jpg`
     const coverUrl512 = 
     `https://uploads.mangadex.org/covers/${manga.id}/${cover?.attributes?.fileName}.512.jpg`
     return {
+        coverUrl,
         coverUrl256,
         coverUrl512
     }
