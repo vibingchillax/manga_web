642c5d12d60edff325aaedd290678b872deed421
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-08-13 15:53:18 +0700

    feat: add more romanized titles to useMangaTitle

--- app/composables/useMangaTitle.ts
+++ app/composables/useMangaTitle.ts
@@ -1,6 +1,10 @@
 import type { Manga } from "~~/shared/types/types"
 
 export const useMangaTitle = (manga: Manga) => {
-  const title = manga.attributes?.title?.en ?? manga.attributes?.title?.["ja-ro"] ?? "Untitled"
+  const title = manga.attributes?.title?.en ?? 
+  manga.attributes?.title?.["ja-ro"] ?? 
+  manga.attributes?.title?.["ko-ro"] ?? 
+  manga.attributes?.title?.["zh-ro"] ?? 
+  "Untitled"
   return title
 }
