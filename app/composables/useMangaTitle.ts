888d2096abf38b2d43138ca831a4671361a9ba9f
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-08-26 10:59:03 +0700

    fix: add ja to useMangaTitle

--- app/composables/useMangaTitle.ts
+++ app/composables/useMangaTitle.ts
@@ -2,6 +2,7 @@ import type { Manga } from "~~/shared/types/types"
 
 export const useMangaTitle = (manga: Manga) => {
   const title = manga.attributes?.title?.en ?? 
+  manga.attributes?.title?.ja ??
   manga.attributes?.title?.["ja-ro"] ?? 
   manga.attributes?.title?.["ko-ro"] ?? 
   manga.attributes?.title?.["zh-ro"] ?? 
