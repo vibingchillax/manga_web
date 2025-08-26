fb86e47a6b9bf8715a03b793503637638861205f
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-08-26 16:03:07 +0700

    feat: add coverOnly manga card

--- app/components/manga/card/index.vue
+++ app/components/manga/card/index.vue
@@ -3,7 +3,11 @@ import { useMangaAuthor } from '~/composables/useMangaAuthor';
 import { useMangaCover } from '~/composables/useMangaCover';
 import type { Manga } from '~~/shared/types/types';
 
-const props = defineProps<{ manga: Manga, use512?: boolean }>()
+const props = defineProps<{
+  manga: Manga,
+  type: 'normal' | 'dense' | 'coverOnly'
+  use512?: boolean
+}>()
 const manga = props.manga
 const title = useMangaTitle(manga);
 const detailsUrl = `/title/${manga.id}/${toKebabCase(title)}`
@@ -15,33 +19,33 @@ const tags = manga.attributes?.tags!
 </script>
 
 <template>
-
-  <NuxtLink class="manga-card-dense" :href="detailsUrl">
-    <div class="manga-card">
-      <NuxtLink class="font-bold title" :href="detailsUrl" style="grid-area: title;">
-        <span>{{ title }}</span>
+  <div class="manga-card" :class="{
+    'dense': type === 'dense',
+    'cover-only': type === 'coverOnly'
+  }">
+    <NuxtLink class="font-bold title" :href="detailsUrl" style="grid-area: title;">
+      <span>{{ title }}</span>
+    </NuxtLink>
+    <span class="author">
+    </span>
+    <div class="manga-card-cover" style="grid-area: art;">
+      <NuxtLink class="group flex items-start relative mb-auto select-none aspect cover" :href="detailsUrl">
+        <NuxtImg class="rounded shadow-md w-full h-auto" :src="use512 ? coverUrl512 : coverUrl256"></NuxtImg>
       </NuxtLink>
-      <span class="author">
-      </span>
-      <div class="manga-card-cover" style="grid-area: art;">
-        <NuxtLink class="group flex items-start relative mb-auto select-none aspect cover" :href="detailsUrl">
-          <NuxtImg class="rounded shadow-md w-full h-auto" :src="use512 ? coverUrl512 : coverUrl256"></NuxtImg>
-        </NuxtLink>
-      </div>
-      <div style="grid-area: status;" class="flex flex-wrap status mb-auto">
-        <MangaTag class="lift dot" mode="status" :status="publicationStatus" />
-      </div>
-      <MangaTagsRow class="tags" :tags="tags" :contentRating="contentRating"></MangaTagsRow>
-      <div class="stats" style="grid-area: stats;"></div>
-      <div class="description !py-0" style="grid-area: description;">
-        <div class="md-container dense">
-          <p>
-            {{ manga.attributes?.description?.en }}
-          </p>
-        </div>
+    </div>
+    <div style="grid-area: status;" class="flex flex-wrap status mb-auto">
+      <MangaTag class="lift dot" mode="status" :status="publicationStatus" />
+    </div>
+    <MangaTagsRow class="tags" :tags="tags" :contentRating="contentRating"></MangaTagsRow>
+    <div class="stats" style="grid-area: stats;"></div>
+    <div class="description !py-0" style="grid-area: description;">
+      <div class="md-container dense">
+        <p>
+          {{ manga.attributes?.description?.en }}
+        </p>
       </div>
     </div>
-  </NuxtLink>
+  </div>
 </template>
 <style lang="css" scoped>
 .manga-card {
@@ -91,12 +95,114 @@ const tags = manga.attributes?.tags!
   }
 }
 
+.manga-card-cover-only .manga-card .title,
+.manga-card.cover-only .title {
+  background: linear-gradient(180deg, transparent, rgba(0, 0, 0, .8));
+  bottom: 0;
+  display: -webkit-box;
+  left: 0;
+  margin-right: 0 !important;
+  overflow: hidden;
+  padding: 1rem .5rem .25rem;
+  position: absolute;
+  text-shadow: 0 0 3px #000;
+  width: 100%;
+  z-index: 1;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+}
+
+@media (min-width: 40rem) {
+
+  .manga-card-cover-only .manga-card .title,
+  .manga-card.cover-only .title {
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+  }
+}
+
+@media (min-width: 48rem) {
+
+  .manga-card-cover-only .manga-card .title,
+  .manga-card.cover-only .title {
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 4;
+  }
+}
+
+@media (min-width: 64rem) {
+
+  .manga-card-cover-only .manga-card .title,
+  .manga-card.cover-only .title {
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 5;
+  }
+}
+
+@media (min-width: 40rem) {
+
+  .manga-card-cover-only .manga-card .title,
+  .manga-card.cover-only .title {
+    font-size: 1rem;
+    line-height: 1.375;
+    line-height: 1.5rem;
+    padding: 1rem .5rem .5rem;
+  }
+}
+
+
+
 .manga-card-dense .manga-card .title,
 .manga-card.dense .title {
   height: 1.5em;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+}
+
+.manga-card-cover-only .manga-card,
+.manga-card.cover-only {
+  border-radius: .25rem;
+  display: block;
+  height: unset;
+  overflow: hidden;
+  padding: 0;
+}
+
+.manga-card-cover-only .manga-card .description,
+.manga-card-cover-only .manga-card .stats,
+.manga-card-cover-only .manga-card .status,
+.manga-card-cover-only .manga-card .tags,
+.manga-card.cover-only .description,
+.manga-card.cover-only .stats,
+.manga-card.cover-only .status,
+.manga-card.cover-only .tags {
+  display: none;
+}
+
+@media (min-width: 40rem) {
+
+  .manga-card-cover-only .manga-card .title,
+  .manga-card.cover-only .title {
+    font-size: 1rem;
+    line-height: 1.375;
+    line-height: 1.5rem;
+    padding: 1rem .5rem .5rem;
+  }
+}
+
+.manga-card-cover-only .manga-card .manga-card-cover,
+.manga-card.cover-only .manga-card-cover {
+  height: 0;
+  padding-bottom: 142.307692%;
+  position: relative;
+  width: 100%;
 }
 
 .author {
--- app/components/manga/details/content/Related.vue
+++ app/components/manga/details/content/Related.vue
@@ -1,16 +1,39 @@
 <script setup lang="ts">
 const props = defineProps<{ manga: Manga }>()
 const { related, pending, error } = await useRelatedMangas(props.manga)
+const items = ref([
+  {
+    icon: 'i-lucide-list',
+    value: 'dense'
+  },
+  {
+    icon: 'i-lucide-rows-2',
+    value: 'normal'
+  },
+  {
+    icon: 'i-lucide-grid-2x2',
+    value: 'coverOnly'
+  }
+]);
+const active = ref<'dense' | 'normal' | 'coverOnly'>('coverOnly');
 </script>
 <template>
   <div class="grid gap-4">
     <div class="font-bold">Related Titles</div>
+    <div class="flex flex-row justify-between gap-6">
+      <div>
+      </div>
+      <UTabs v-model="active" :items="items" />
+    </div>
     <div v-if="pending">Loading</div>
     <div v-if="error">{{ error }}</div>
     <div v-else class="min-w-0" v-for="r in related" :key="r.type">
       <div class="font-medium mb-2">{{ r.label }}</div>
-      <div class="grid gap-2 grid-cols-2">
-        <manga-card v-for="m in r.mangas" :key="m?.id" :manga="m!" />
+      <div class="grid gap-2" :class="{
+        'grid-cols-2': active === 'normal',
+        'grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3': active === 'coverOnly'
+      }">
+        <manga-card v-for="m in r.mangas" :key="m?.id" :manga="m!" :type="active" />
       </div>
     </div>
   </div>
