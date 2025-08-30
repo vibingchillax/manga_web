f72621738d79755043ba82410a64c72a996dc122
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-08-30 16:15:03 +0700

    refactor: use chapterId only for reading history

--- app/stores/useReadingHistoryStore.ts
+++ app/stores/useReadingHistoryStore.ts
@@ -1,13 +1,10 @@
+const length = 69420 //mangadex picked this not my fault
+
 export interface ReadingHistory {
   _readingHistory: {
     chapterId: string
     time: string
-  }[] | {
-    titleEntry?: string
-    sourceId: string
-    title: string
-    chapterId: string
-    time: string
+    type: 'uploaded' | 'scraped'
   }[]
 }
 
@@ -15,6 +12,23 @@ export const useReadingHistoryStore = defineStore('readingHistory', {
   state: (): ReadingHistory => ({
     _readingHistory: []
   }),
+  actions: {
+    pushChapterRead(entry: { chapterId: string; type: 'uploaded' | 'scraped' }) {
+      this._readingHistory = this._readingHistory.filter(
+        item => !(item.chapterId === entry.chapterId && item.type === entry.type)
+      )
+
+      this._readingHistory.unshift({
+        chapterId: entry.chapterId,
+        time: new Date().toISOString(),
+        type: entry.type
+      })
+
+      if (this._readingHistory.length > length) {
+        this._readingHistory.splice(length)
+      }
+    }
+  },
   persist: {
     storage: localStorage
   }
