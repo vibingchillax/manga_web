6c74cde7bc9cd418c0c4be130b9ee2b51b3a8d58
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-09-22 20:45:07 +0700

    dev: make temp logo smaller

--- app/components/navbar/Logo.vue
+++ app/components/navbar/Logo.vue
@@ -6,7 +6,7 @@ const layout = useLayout()
     <UButton class="mr-2" icon="i-lucide-text-align-start" @click="layout.setMenu(true)" variant="ghost" />
     <NuxtLink to="/">
       <!-- weird temp logo plz put logo-->
-      <USkeleton class="h-12 w-48 flex justify-center items-center">manga_web</USkeleton>
+      <USkeleton class="h-9 w-40 flex justify-center items-center">manga_web</USkeleton>
     </NuxtLink>
   </div>
 </template>
