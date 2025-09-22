730aadd35448db1290d50d3614c14afcb1c76ecb
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-09-22 20:13:01 +0700

    feat: require auth prop for page component

--- app/components/Page.vue
+++ app/components/Page.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 const router = useRouter()
-defineProps({
+const auth = useAuth()
+const authRedirect = useAuthRedirect()
+const route = useRoute()
+const props = defineProps({
   title: {
     type: String,
     required: true
@@ -12,6 +15,10 @@ defineProps({
   noBack: {
     type: Boolean,
     default: false
+  },
+  requireAuth: {
+    type: Boolean,
+    default: false
   }
 })
 </script>
@@ -21,6 +28,19 @@ defineProps({
       <UButton class="mr-4" icon="i-lucide-arrow-left" @click="router.back()" variant="ghost" />
       <h2 class="font-header text-2xl font-semibold">{{ title }}</h2>
     </div>
-    <slot></slot>
+    <div v-if="requireAuth && !auth.session.value?.isAuthenticated">
+      <h2 class="text-center text-lg font-semibold mt-2 mb-4">You need to sign in to access this page.</h2>
+      <div class="flex justify-center gap-4">
+        <UButton label="Log in" @click="() => {
+          authRedirect = route.fullPath
+          router.push('/login')
+        }"/>
+        <UButton color="neutral" label="Register" @click="() => {
+          authRedirect = route.fullPath
+          router.push('/register')
+        }"/>
+      </div>
+    </div>
+    <slot v-else></slot>
   </div>
 </template>
