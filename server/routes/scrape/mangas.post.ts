cad9dc8b977ac8004e5c0d2af3010f64f1ef457f
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-10-03 22:19:34 +0700

    feat: validate with zod for endpoints that use body

--- server/routes/scrape/mangas.post.ts
+++ server/routes/scrape/mangas.post.ts
@@ -1,35 +1,44 @@
-type MangaRequestBody = {
-  sourceId: string
-  title: string
-  mangadexId: string
-}
+import * as z from 'zod'
+
+const scrapeMangaSchema = z.object({
+  sourceId: z.string()
+    .min(2, { message: "sourceId must be at least 2 characters" })
+    .max(40, { message: "sourceId can not exceed 40 characters"} ).trim(),
+  title: z.string()
+    .min(1,{ message: "title must be at least 1 character" })
+    .max(500, { message: "title can not exceed 500 characters" }).trim(),
+  mangadexId: z.string().uuid({ message: "mangadexId must be a valid UUID" })
+})
 
 export default defineEventHandler(async (event) => {
-  const body: MangaRequestBody = await readBody(event)
-  if (!body.sourceId || !body.title || !body.mangadexId) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: 'Missing required parameters: title, sourceId, mangadexId'
-    })
-  }
+  const body = scrapeMangaSchema.safeParse(await readBody(event))
+
+  if (!body.success) throw createError({
+    statusCode: 400,
+    statusMessage: 'Invalid request data',
+    data: body.error.flatten()
+  })
+
+  const data = body.data
+
   try {
     const result = await prisma.scrapedMangas.findMany({
       where: {
-        sourceId: body.sourceId,
-        mangaDexId: body.mangadexId
+        sourceId: data.sourceId,
+        mangaDexId: data.mangadexId
       }
     })
 
     if (!(result.length > 0)) {
-      return await refreshMangas(body)
+      return await refreshMangas(data)
     }
 
     const lastUpdated = result[0].updatedAt
     const stale = Date.now() - lastUpdated.getTime() > 1000 * 60 * 60
 
     if (stale) {
-      refreshMangas(body).catch((err: any) => {
-        console.error(`Background refresh failed for manga title ${body.title} (${body.sourceId})`, err)
+      refreshMangas(data).catch((err: any) => {
+        console.error(`Background refresh failed for manga title ${data.title} (${data.sourceId})`, err)
       })
     }
 
