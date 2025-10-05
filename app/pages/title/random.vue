7549a9a0b3d8b33518b85a1eee8057207cd6cf49
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-10-05 17:20:32 +0700

    fix: random title now works

--- app/pages/title/random.vue
+++ app/pages/title/random.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 const router = useRouter()
+const { contentRating } = storeToRefs(usePreferencesStore())
+const { data: response, pending, error } = await useMangadex('/manga/random', {
+  query: {
+    'contentRating[]': contentRating.value
+  }
+})
 
-const { data, error } = await useAsyncData('random', () => getMangaRandom())
-
-if (data.value?.data) {
-  router.push(`/title/${data.value.data.id}/${toKebabCase(data.value.data.attributes?.title?.en as string)}`)
+if (response.value?.data) {
+  router.push(useManga(response.value?.data).detailsUrl.value)
 }
 </script>
 
 <template>
-  <div>Loading random manga...</div>
+  <div v-if="pending">Loading random manga...</div>
+  <div v-else-if="error">{{ error }}</div>
 </template>
