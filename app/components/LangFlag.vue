86abc7b288bc4e8fb3bfd34c31493a8d6cced74d
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-10-12 22:30:22 +0700

    feat: lang flag component chore: add flag assets

---
+++ app/components/LangFlag.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+const props = withDefaults(defineProps<{
+  lang: string
+  size?: number
+  displayScripts?: boolean
+}>(), {
+  size: 24,
+  displayScripts: false,
+})
+
+const languages = LANGUAGES_BY_CODE
+
+const halfSize = computed(() => Math.floor(props.size / 2))
+const flag = computed(() => languages.get(props.lang) || languages.get('NULL'))
+</script>
+<template>
+  <div
+    :is="'div'"
+    class="select-none"
+    :wrap="displayScripts"
+    :style="[
+      { display: 'inline-block !important' },
+      { minWidth: size + 'px', minHeight: size + 'px' }
+    ]"
+    v-bind="$attrs"
+  >
+    <img
+      v-bind="$attrs"
+      v-if="flag"
+      :class="!displayScripts && 'inline-block select-none'"
+      :title="flag.name.inEnglish"
+      :src="`/img/flags/${flag.display.flag}.svg`"
+      :alt="`${flag.name.inEnglish} flag icon`"
+      :width="size"
+      :height="size"
+    />
+
+    <img
+      v-if="displayScripts && flag?.display.script"
+      :title="flag.name.inEnglish"
+      :src="`/img/scripts/${flag.display.script}.svg`"
+      :alt="`${flag.name.inEnglish} script icon`"
+      :width="halfSize"
+      :height="halfSize"
+      style="margin-top: -12px; margin-left: auto; margin-right: -2px;"
+    />
+  </div>
+</template>
