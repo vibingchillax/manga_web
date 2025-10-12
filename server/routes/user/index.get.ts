69ff6bd22f17d14e523eb64a9787bf339e4eb69f
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-10-12 17:51:39 +0700

    fix: return total user count correctly in pagination

--- server/routes/user/index.get.ts
+++ server/routes/user/index.get.ts
@@ -3,8 +3,8 @@ import * as z from 'zod'
 const querySchema = z.object({
   limit: z.coerce.number().min(0).max(100).optional(),
   offset: z.coerce.number().optional(),
-  'ids[]': z.
-    union([z.string().uuid(), z.array(z.string().uuid())])
+  'ids[]': z
+    .union([z.string().uuid(), z.array(z.string().uuid())])
     .optional()
     .transform(val => {
       if (!val) return undefined
@@ -15,7 +15,7 @@ const querySchema = z.object({
 })
 
 export default defineEventHandler(async (event) => {
-  const user = getAuthenticatedUser(event)
+  const user = await getAuthenticatedUser(event)
 
   if (!user) throw createError({
     statusCode: 401,
@@ -24,26 +24,35 @@ export default defineEventHandler(async (event) => {
 
   const query = await getValidatedQuery(event, querySchema.safeParse)
 
-  const users = await prisma.user.findMany({
-    take: query.data?.limit ?? 10,
-    skip: query.data?.offset ?? 0,
-    where: {
-      id: query.data?.['ids[]'] ? { in: query.data['ids[]'] } : undefined,
-      username: query.data?.username ? { contains: query.data.username } : undefined
-    },
-    orderBy: query.data?.['order[username]'] ? { username: query.data['order[username]'] } : undefined,
-    select: {
-      id: true,
-      username: true,
-      role: true
-    }
-  })
+  const filters = {
+    id: query.data?.['ids[]'] ? { in: query.data['ids[]'] } : undefined,
+    username: query.data?.username
+      ? { contains: query.data.username }
+      : undefined
+  }
+
+  const [users, total] = await Promise.all([
+    prisma.user.findMany({
+      take: query.data?.limit ?? 10,
+      skip: query.data?.offset ?? 0,
+      where: filters,
+      orderBy: query.data?.['order[username]']
+        ? { username: query.data['order[username]'] }
+        : undefined,
+      select: {
+        id: true,
+        username: true,
+        role: true
+      }
+    }),
+    prisma.user.count({ where: filters })
+  ])
 
   return {
     result: "ok",
     data: users,
     limit: query.data?.limit ?? 10,
     offset: query.data?.offset ?? 0,
-    count: users.length
+    count: total
   }
 })
