029766996562a99a8af11ebd311965c236d5b169
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-10-16 22:54:25 +0700

    feat: overlay component

---
+++ app/components/Overlay.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  zIndex?: string | number;
+  opacity?: string | number;
+  attach?: string | HTMLElement | null;
+}>(), {
+  zIndex: 100,
+  opacity: 0.7,
+  attach: null,
+})
+
+const overlayStyle = computed(() => ({
+  '--overlay-opacity': props.opacity,
+  zIndex: props.zIndex,
+}))
+</script>
+<template>
+  <Teleport :to="attach" v-if="attach">
+    <div ref="content" class="mw-overlay text-white p-8 flex flex-col items-center justify-center"
+      :style="[{ zIndex }, overlayStyle]" @click="$emit('click', $event)">
+      <slot></slot>
+    </div>
+  </Teleport>
+  <div v-else ref="content" class="mw-overlay text-white p-8 flex flex-col items-center justify-center"
+    :style="{ zIndex }" @click="$emit('click', $event)">
+    <slot></slot>
+  </div>
+</template>
+<style lang="css" scoped>
+.mw-overlay {
+  position: fixed
+}
+
+.mw-overlay,
+.mw-overlay:before {
+  height: 100%;
+  left: 0;
+  top: 0;
+  width: 100%
+}
+
+.mw-overlay:before {
+  background-color: rgb(var(--mw-accent-10));
+  content: "";
+  display: block;
+  opacity: var(--overlay-opacity);
+  position: absolute;
+  z-index: -1
+}
+</style>
