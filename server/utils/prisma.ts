3e76f9e42dc45666c13c07f5ec12ce31cdd725e0
vibingchillax <235709660+vibingchillax@users.noreply.github.com>
2025-12-27 15:35:05 +0700

    deps: bump prisma to v7 feat: add rejected state to manga draft

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from "#shared/prisma/client";
 import { getDatabaseUrl } from "#shared/dbUrl";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const adapter = new PrismaPg({
+  connectionString: getDatabaseUrl(),
+});
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    datasourceUrl: getDatabaseUrl(),
+    adapter
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
